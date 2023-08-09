e7eba53c8009e236c1d8d3698a40a3c12a98ac33
Alex Jercan <jercan_alex27@yahoo.com>
2023-08-09 08:44:00 +0300

    feat: added optional argument

--- fried.sh
+++ fried.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
 url=$1
+url_optional=$2
 minecraft=https://www.youtube.com/watch?v=NX-i0IWl3yg
 video1=input.mp4
 video2=minecraft.mp4
 
-if [ -z "$url" ]
-then
-    echo "Usage: ./script.sh <video-url>"
+if [ -z "$url" ]; then
+    echo "Usage: ./script.sh <video-url> [optional-video-url]"
     exit 1
 fi
 
+if [ -z "$url_optional" ]; then
+    url_optional=$minecraft
+fi
+
 if [ -f "$url" ]; then
+    echo "Using $url as input"
     video1=$url
 else
+    echo "Downloading $url"
     rm -f $video1
     youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' $url -o $video1
 fi
 
-if [ ! -f "$video2" ]
-then
-    echo "Downloading minecraft.mp4"
-    youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' $minecraft -o $video2
+if [ -f "$url_optional" ]; then
+    echo "Using $url_optional as optional video"
+    video2=$url_optional
+else
+    if [ ! -f "$video2" ]; then
+        echo "Downloading $url_optional"
+        youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' $url_optional -o $video2
+    else
+        echo "minecraft.mp4 already exists; if you want to redownload using url_optional, delete minecraft.mp4"
+    fi
 fi
 
 duration_1=$(ffprobe -i $video1 -show_entries format=duration -v quiet -of csv="p=0")
